8b99d80d738ec0a60f3629ba2d01ce3fdf2e975f
Your Name <me@mchlwrrn.com>
2014-11-18 07:26:45 +0000

    Added functionality to the submit button

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,6 +6,18 @@ class PlacesController < ApplicationController
 	def new
 		@place = Place.new
 	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
+
 end
 
 #class PlacesController < ApplicationController
