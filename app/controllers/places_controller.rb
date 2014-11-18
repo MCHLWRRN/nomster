f60d9c446529a23e1b579af1be084625f9a88833
Your Name <me@mchlwrrn.com>
2014-11-18 18:52:00 +0000

    Specified user ability to destroy only places created by user

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
 	def index
 		@places = Place.paginate(:page => params[:page], :per_page => 5)
@@ -31,12 +31,16 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
+
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.destroy
 		redirect_to root_path
 	end
