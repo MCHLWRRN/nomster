8923612a78273c63ea1f7607c36226eaadc14483
Your Name <me@mchlwrrn.com>
2014-11-18 19:01:51 +0000

    Added validation for place name

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
 	def show
@@ -33,7 +37,11 @@ class PlacesController < ApplicationController
 		end
 
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
 	end
 
 	def destroy
@@ -41,6 +49,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
 		end
+
 		@place.destroy
 		redirect_to root_path
 	end
