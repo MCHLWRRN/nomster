506734a1a3ba0255ec004f12d36b121f120c3c96
Your Name <me@mchlwrrn.com>
2014-11-19 22:36:19 +0000

    Added comments feature

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
+	
 	validates :name, :presence => true, length: { minimum: 3 }
 	validates :address, :presence => true
 	validates :description, :presence => true
