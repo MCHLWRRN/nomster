1c2df7d97eecf22e9d5e20d672391dd87109396b
Your Name <me@mchlwrrn.com>
2014-11-27 05:27:07 +0000

    Added Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Nomster::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
   config.action_mailer.default_url_options = { :host => 'nomster-michael-warren.herokuapp.com'}
 
   # Code is not reloaded between requests.
